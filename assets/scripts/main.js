a4b4c9c52e18038e4b9ae3ed799d3b756e6d0ca4
CodyConck <codyconck@gmail.com>
2022-10-18 18:49:25 -0500

    added location finding

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,26 +10,24 @@ class App {
       document.getElementById(input)
     );
     this.places = new window.google.maps.places.PlacesService(
-      document.createElement('div')
+      document.createElement("div")
     );
 
     this.store = store;
 
     const handleChange = () => {
       const place = this.autocomplete.getPlace();
-      // console.log({ place });
+      console.log({ place });
       this.store.setPlace(place);
-    }
+    };
 
-    this.autocomplete.addListener('place_changed', handleChange)
-
+    this.autocomplete.addListener("place_changed", handleChange);
   }
 }
 
-
-
-document.addEventListener('alpine:init', () => {
-  Alpine.store('global', {
+// sets place and returns image to card
+document.addEventListener("alpine:init", () => {
+  Alpine.store("global", {
     place: null,
     setPlace(place) {
       this.place = place;
@@ -38,16 +36,15 @@ document.addEventListener('alpine:init', () => {
       if (this.place) {
         return place.photos[0].getUrl();
       }
-      return '/assets/images/Image_Clubhouse.jpg';
+      return "/assets/images/Image_Clubhouse.jpg";
     },
-  })
-  const app = window.app = new App({
-    Alpine,
-    store: Alpine.store('global'),
-    input: 'search'
   });
+  const app = (window.app = new App({
+    Alpine,
+    store: Alpine.store("global"),
+    input: "search",
+  }));
 });
-
 
 function parsePlace(place = {}) {
   var byType = (place.address_components || []).reduce(function (acc, data) {
@@ -59,40 +56,55 @@ function parsePlace(place = {}) {
 
   var result = {
     name: getName(place),
-    address_1: `${placeGet('street_number')} ${placeGet('route')}`,
+    address_1: `${placeGet("street_number")} ${placeGet("route")}`,
     city:
-      placeGet('locality') ||
-      placeGet('sublocality') ||
-      placeGet('sublocality_level_1') ||
-      placeGet('neighborhood') ||
-      placeGet('administrative_area_level_3') ||
-      placeGet('administrative_area_level_2'),
-    state: placeGet('administrative_area_level_1', true),
-    country: placeGet('country', true),
-    postal_code: placeGet('postal_code'),
+      placeGet("locality") ||
+      placeGet("sublocality") ||
+      placeGet("sublocality_level_1") ||
+      placeGet("neighborhood") ||
+      placeGet("administrative_area_level_3") ||
+      placeGet("administrative_area_level_2"),
+    state: placeGet("administrative_area_level_1", true),
+    country: placeGet("country", true),
+    postal_code: placeGet("postal_code"),
     latitude: place.geometry ? place.geometry.location.lat() : undefined,
     longitude: place.geometry ? place.geometry.location.lng() : undefined,
-    google_place_id: place.id || place.place_id
+    google_place_id: place.id || place.place_id,
   };
 
   return result;
 
   function getName(place) {
-    return place.types.includes('establishment') ? place.name : null;
+    return place.types.includes("establishment") ? place.name : null;
   }
 
   function placeGet(key, short) {
-    if (!(key in byType)) return '';
+    if (!(key in byType)) return "";
 
     return short ? byType[key].short_name : byType[key].long_name;
   }
 }
 
+const x = document.getElementById("demo");
 
+function getLocation() {
+  try {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } catch {
+    x.innerHTML = err;
+  }
+}
 
+function showPosition(position) {
+  x.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+}
+
+// return users location coordinates on search
 
 // (function myFunction() {
-
-
 
 // }())
